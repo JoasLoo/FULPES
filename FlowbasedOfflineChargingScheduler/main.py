b06b731a279ad8343c95931b2ac803f2f41909a3
JoasLoo <joas@loonstra.eu>
2025-04-23 16:12:55 +0200

    added timestamps again

--- FlowbasedOfflineChargingScheduler/main.py
+++ FlowbasedOfflineChargingScheduler/main.py
@@ -45,6 +45,8 @@ from LP import LP
 from FOCS import FOCS, FlowNet, FlowOperations, FOCSinstance
 from Bookkeeping import Bookkeeping
 
+t0 = time.perf_counter()
+
 instanceSize = 10 #number of EVs/jobs in instance
 timeStep = 900 #quarterly granularity
 maxFlowAlg = shortest_augmenting_path #alternatively use e.g., edmonds_karp, preflow_push, or dinitz
@@ -53,23 +55,43 @@ randomSample = True
 # Real Training data
 instanceData = pd.read_excel('../Data/ev_session_data_OR.xlsx')
 
+t1 = time.perf_counter()
+
 if not randomSample:
     instance = FOCSinstance(instanceData[:instanceSize], timeStep)
 if randomSample:
     sample = sorted(random.sample(range(0,len(instanceData)), instanceSize))
     instance = FOCSinstance(instanceData.iloc[sample], timeStep)  
-
+t2 = time.perf_counter()
 '''--------------start FOCS--------------'''
 flowNet = FlowNet()
+t3 = time.perf_counter()
 flowNet.focs_instance_to_network(instance)
+t4 = time.perf_counter()
 flowOp = FlowOperations(flowNet.G, instance)
+t5 = time.perf_counter()
 focs = FOCS(instance, flowNet, flowOp)
+t6 = time.perf_counter()
 focs.flow_func = shortest_augmenting_path
+t7 = time.perf_counter()
 f = focs.solve_focs(MPCstopper=False, MPCcondition=0)
+t8 = time.perf_counter()
 
 obj_val = focs.objective()
+t9  = time.perf_counter()
 print('FOCS objective value = ', obj_val)
 print('FOCS flow (schedule in middle edge layer): \n', focs.f)
+
+print(f"1) Data loading took            {t1 - t0:.3f} seconds")
+print(f"2) Instance creation took       {t2 - t1:.3f} seconds")
+print(f"3) FlowNet instantiation took   {t3 - t2:.3f} seconds")
+print(f"4) Building flow network took   {t4 - t3:.3f} seconds")
+print(f"5) FlowOperations instantiation took {t5 - t4:.3f} seconds")
+print(f"6) FOCS instantiation took      {t6 - t5:.3f} seconds")
+print(f"7) Setting flow_func took       {t7 - t6:.3f} seconds")
+print(f"8) FOCS solve took              {t8 - t7:.3f} seconds")
+print(f"9) Objective calculation took   {t9 - t8:.3f} seconds")
+
 
 #optional operations
 #instance.toy_instance_2() #overwrites (potentially empty) FOCS instance with toy instance
