21404c7e55b0b907cb6b591aa49b66892d40eae4
JoasLoo <148538684+JoasLoo@users.noreply.github.com>
2025-05-22 12:47:40 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,10 @@
 @echo off
 
+<<<<<<< Updated upstream
 g++ -O3 -ffast-math -static-libstdc++ -funroll-loops -g -o main main.cpp
+=======
+g++ -O3 -ffast-math -static-libstdc++ -funroll-loops -o main main.cpp
+>>>>>>> Stashed changes
 
 
 if %ERRORLEVEL% EQU 0 (
